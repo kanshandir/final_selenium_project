4363431e834f3a1cd80a6602bbe5984868cb60fe
Kantemir Shandirov <kan.shandir94@gmail.com>
2021-02-07 15:22:56 +0300

    add new methods in login page and main page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,3 @@
-from lib2to3.pgen2 import driver
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
@@ -10,15 +9,13 @@ class LoginPage(LoginPageLocators, BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "/login" in driver.current_url(self.url), "login is absent in current url"
+        assert "login" in self.browser.current_url, "login is absent in current url"
         # реализуйте проверку на корректный url адрес
 
     def should_be_login_form(self):
-        assert self.LOGIN_FORM > 0, "Login link is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
         # реализуйте проверку, что есть форма логина
 
     def should_be_register_form(self):
-        assert self.REGISTRATION_FORM > 0, "Registration link is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
         # реализуйте проверку, что есть форма регистрации на странице
-
-
--- pages/main_page.py
+++ pages/main_page.py
@@ -6,6 +6,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
